ea8a337ac3658e4e5bd3dd7bea041c0fb3fa5af7
何辰风 <2583130081@qq.com>
2024-06-22 04:34:40 +0800

    更新 Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -43,7 +43,7 @@
 <script setup>
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
-import { Link, Blog, CompactDisc, Compass，BrandBing，Fire，Fish，LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
+import { Link, Blog, CompactDisc, Compass,BrandBing,Fire,Fish,LaptopCode } from "@vicons/fa"; // 注意使用正确的类别
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper/modules";
@@ -66,8 +66,8 @@ const siteIcon = {
   Blog,
   CompactDisc,
   Compass,
-  BrandBing，
-  Fire，
+  BrandBing,
+  Fire,
   Fish,
   LaptopCode,
 };
